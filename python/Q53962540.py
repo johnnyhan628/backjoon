303a16abffd578301436c1c8b7bce0c6447bdad1
johnnyhan628 <johnnyhan628@gmail.com>
2024-08-02 01:36:26 +0900

    백준 파이썬 반복문(1)

---
+++ python/Q53962540.py
@@ -0,0 +1,13 @@
+N = int(input())
+i = N//4
+str = 'int'
+for _ in range(i):
+  str = 'long '+ str
+print(str)
+
+"""
+맘에 드는 다른 사람의 정답
+#N = int(input())
+n = N/4
+print("long "*int(n) + "int")
+"""
