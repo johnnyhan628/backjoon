71ed84c416625eaea8e65e47c508a5d606d33317
johnnyhan628 <johnnyhan628@gmail.com>
2025-07-31 19:08:45 +0900

    골드바흐파티션_2

--- java/Q17103.java
+++ java/Q17103.java
@@ -5,27 +5,29 @@ public class Q17103 {
     BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
     int T = Integer.parseInt(bf.readLine());
 
-    int count = 0; //각 케이스의 count 초기화
-    //T개의 테스트 수만큼 반복
+    int count = 0;
+
     for(int i=0; i<T; i++){
-      int N = Integer.parseInt(bf.readLine()); //짝수 값을 받음
-      
-      for(int prime1 = 2; prime1 <= N; prime1++){
+      //수 받기
+      int N = Integer.parseInt(bf.readLine());
+
+      for(int prime1 = 2; prime1 <= N/2+1; prime1++){
         int prime2 = N-prime1;
 
-        if(isPrime(prime1) && isPrime(prime2)){
+        if(isPrime(prime1)){
+          if(isPrime(prime2))
           count++;
         }
       }
+      System.out.println(count);
     }
-    System.out.println(count);
   }
 
   public static boolean isPrime(int n) {
-        if (n < 2) return false;
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) return false;
-        }
-        return true;
+    if (n < 2) return false;
+    for (int i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i == 0) return false;
     }
+    return true;
+  }
 }
