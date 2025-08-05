6a913924ec2cfa40dd05fcad861c9fca5def5fc6
johnnyhan628 <johnnyhan628@gmail.com>
2025-08-06 00:28:42 +0900

    창문닫기(2)

--- java/Q13909.java
+++ java/Q13909.java
@@ -6,12 +6,13 @@ public class Q13909 {
          * N번째 사람은 N의 배수 번째 창문을 열거나 닫음
          * 최초 배열의 길이는 N이며 모든 배열의 인덱스 값은 false
          * N번째 사람까지 진행한 후 모든 배열의 true count를 구하면 됨.
+         * 2이상 N이하의 수 중에 소수가 아닌 수의 개수를 구하면 됨
          */
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(bf.readLine());
-        
+
 
     }
 }
