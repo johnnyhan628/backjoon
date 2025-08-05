3b30869d13136df9e76eb3d2bfa87ce861d305bd
johnnyhan628 <johnnyhan628@gmail.com>
2025-08-05 21:52:38 +0900

    창문닫기(1)

---
+++ java/Q13909.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Q13909 {
+    public void main(String[] args) throws IOException{
+        /**
+         * N번째 사람은 N의 배수 번째 창문을 열거나 닫음
+         * 최초 배열의 길이는 N이며 모든 배열의 인덱스 값은 false
+         * N번째 사람까지 진행한 후 모든 배열의 true count를 구하면 됨.
+         */
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+        
+
+    }
+}
