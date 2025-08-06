e25d5574c227482df5ff3a61b83d954cda825766
johnnyhan628 <johnnyhan628@gmail.com>
2025-08-06 17:28:13 +0900

    창문닫기 마무리

--- java/Q13909.java
+++ java/Q13909.java
@@ -1,18 +1,19 @@
 import java.io.*;
 
 public class Q13909 {
-    public void main(String[] args) throws IOException{
-        /**
-         * N번째 사람은 N의 배수 번째 창문을 열거나 닫음
-         * 최초 배열의 길이는 N이며 모든 배열의 인덱스 값은 false
-         * N번째 사람까지 진행한 후 모든 배열의 true count를 구하면 됨.
-         * 2이상 N이하의 수 중에 소수가 아닌 수의 개수를 구하면 됨
-         */
-
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
+    /**
+     * 문제 분석이 잘못됨...
+     * 약수의 수가 홀수인 숫자들만 더하면 됨
+     * 약수의 수가 홀수인 숫자는 제곱수로 표현할 수 있는 수 밖에 없음.
+     */
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));    
         int N = Integer.parseInt(bf.readLine());
-
-
+        int count = 0;
+        for(int i=1; i*i<=N; i++){
+            count++;
+        }
+        bw.write(count);
     }
 }
