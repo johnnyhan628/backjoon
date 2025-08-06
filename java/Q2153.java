ff6d5b3453ca213c5ad75b8d6521677fc6866b2c
johnnyhan628 <johnnyhan628@gmail.com>
2025-08-06 18:02:49 +0900

    소수 단어(1)

---
+++ java/Q2153.java
@@ -0,0 +1,46 @@
+import java.io.*;
+
+public class Q2153 {
+  public static void main(String[] args) throws IOException{
+    /*
+     * 0. 알파벳을 숫자로 어떻게 표현하면 좋을까?
+     * 1. 단어를 받는다.
+     * 2. 각 단어를 쪼개서 합한다.
+     * 3. 소수인지 판단한다.
+     */
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String s = br.readLine();
+
+    int count = 0;
+    char[] chars = s.toCharArray();
+
+    for(char c : chars){
+      count = count + changeInteger(c);
+    }
+
+    System.out.println(count);
+
+    if(isPrime(count)){
+      System.out.print("It is a prime word.");
+    }else{
+      System.out.print("It is not a prime word.");
+    }
+  }
+
+  public static boolean isPrime(int n){
+    if(n < 2) return false;
+    for(int i=2; i<=Math.sqrt(n); i++){
+      if(n%i == 0) return false;
+    }
+    return true;
+  }
+
+  public static int changeInteger(char k){
+    if(k >= 'a' && k <= 'z'){
+      return k - 'a' + 1;
+    }else if(k >= 'A' && k <= 'Z'){
+      return k - 'A' + 27;
+    }else throw new IllegalArgumentException();
+  }
+}
