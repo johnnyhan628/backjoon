2779578d8df45116fd23d4b21e862823cdbd6f6d
johnnyhan628 <johnnyhan628@gmail.com>
2025-08-20 21:52:36 +0900

    주몽 - 마무리

--- java/Q1940.java
+++ java/Q1940.java
@@ -1,10 +1,12 @@
 import java.io.*;
-
+import java.util.*;
 public class Q1940 {
   /*
    * 이번에는 투포인터로 풀어보자.!
    * 배열 안의 두 수를 더해서 원하는 값(M)이 만들어지면 해당 두 수는 더이상 사용하면 안될텐데,
-   * 그러면 left ++ 하고 사용하면 안되는 수는 0으로 바꿔버리자.
+   * left++ right-- 하고
+   * 작을 때는 더 큰 수가 필요하니 left++
+   * 클 때는 더 작은 수가 필요하니 right--
    */
   public static void main(String[] args) throws IOException {
 
@@ -17,12 +19,25 @@ public class Q1940 {
       arr[i] = Integer.parseInt(strArr[i]);
     }
 
+    Arrays.sort(arr);
     int left = 0;
-    int right = N - 1;
-
-    while (left > N - 1) {
-
+    int right = N-1;
+    int cnt = 0;
+    
+    while (left < right) {
+      int sum = arr[left] + arr[right];
+      if(sum == M){
+        cnt++;
+        left++;
+        right--;
+      }else if(sum > M){
+        right --;
+      }else{
+        left++;
+      }
     }
+
+    System.out.print(cnt);
 
   }
 }
